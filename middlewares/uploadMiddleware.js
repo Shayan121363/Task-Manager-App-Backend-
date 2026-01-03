09e72ef92f1b0bf483ac18513112391063b44395
Shayan121363 <ahmedshayan440@gmail.com>
2026-01-03 23:28:30 +0500

    Middle ware files

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+// Ensure uploads directory exists
+const uploadDir = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg, .jpg, and .png files are allowed"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+module.exports = upload;
